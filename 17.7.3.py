8acd8686783455de406b3a1e039b0f91edd87121
Sergey Lozovoy <sedmaev@gmail.com>
2024-02-11 15:17:42 +0400

    initial commit

---
+++ 17.7.3.py
@@ -0,0 +1,15 @@
+from decimal import Decimal
+
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+d = input('Введите сумму:')
+money = Decimal(d)
+deposit = []
+
+for bank, rate in per_cent.items():
+    interest = money * Decimal(str(rate)) / Decimal('100')
+    deposit.append(str(interest))
+
+print("Депозиты:", deposit)
+
+max_deposit = max(deposit)
+print("Самое большое число в депозите:", max_deposit)
